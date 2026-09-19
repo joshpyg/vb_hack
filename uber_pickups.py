cf5b34296d1d167d2712cce85c25de4f90655fc9
joshpy <156140203+joshpyg@users.noreply.github.com>
2026-09-19 19:55:55 +0000

    a

--- uber_pickups.py
+++ uber_pickups.py
@@ -3,6 +3,11 @@ import pandas as pd
 import numpy as np
 
 number = st.number_input(
-    "Inert a number", value=None, placeholder="Type a number"
+    "Type a number", value=None, placeholder="Type a number"
+)
+st.write("The current number is ", number)
+
+number = st.number_input(
+    "Type a second number", value=None, placeholder="Type a number"
 )
 st.write("The current number is ", number)
