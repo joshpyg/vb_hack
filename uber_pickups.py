879a08a9d91932a257efdcc1cc615c08cd488298
joshpy <156140203+joshpyg@users.noreply.github.com>
2026-09-19 19:46:50 +0000

    a

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.header('st.button')
-
-if st.button('Say hello'):
-     st.write('Why hello there')
-else:
-     st.write('Goodbye')
+number = st.number_input(
+    "Inert a number", value=None, placeholder="Type a number"
+)
+st.write("The current number is ", number)
