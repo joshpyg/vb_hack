be568b03891d83f09e258fe8814e3bca82f72591
joshpy <156140203+joshpyg@users.noreply.github.com>
2026-09-24 22:35:06 +0000

    h

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,18 +2,11 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-number = st.number_input(
-    "Type a number", value=None, placeholder="Type a number"
+
+option = st.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone"),
 )
-st.write("The current number is ", number)
 
-number2 = st.number_input(
-    "Type a second number", value=None, placeholder="Type a number"
-)
-st.write("The current number is ", number)
+st.write("You selected:", option)
 
-if st.button("add"):
-    st.write(number + number2)
-
-if st.button("subtract"):
-    st.write(number - number2)
