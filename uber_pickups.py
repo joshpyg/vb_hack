90157a76211bbedd5ea1d30d599fb78ccd9becf1
joshpy <156140203+joshpyg@users.noreply.github.com>
2026-09-24 22:43:26 +0000

    s

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,11 +2,28 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-
-option = st.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone"),
+number1 = st.number_input(
+    "Enter the first number", value=None, placeholder = "Type a number..."
 )
+number2 = st.number_input(
+    "Enter the first number", value=None, placeholder = "Type a number..."
+)
+operation = st.selectbox(
+    "choose an oeration",
+    ["Add", "Subtract", "Multiply", "Divide"]
+)
+if number1 is not None and number2 is not None:
 
-st.write("You selected:", option)
+    if operation == "Add":
+        answer = number1 + number2
 
+    elif operation == "Subtract":
+        answer = number1 - number2
+
+    elif operation == "Multiply":
+        answer = number1 * number2
+
+    elif operation == "Divide":
+        answer = number1 / number2
+
+    st.write("Answer:", answer)
