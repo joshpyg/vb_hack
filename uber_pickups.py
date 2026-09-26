78cbaf4068f80689e47dacd279b5af7580ed83af
joshpy <156140203+joshpyg@users.noreply.github.com>
2026-09-26 19:04:08 +0000

    a

--- uber_pickups.py
+++ uber_pickups.py
@@ -19,13 +19,49 @@ if st.button("Search"):
     try:
             res = fetch_pokemon(name)
 
-            st.write("ID:", res["id"])
-            st.write("Name:", res["name"])
-            st.write("Weight:", res["weight"])
-            st.write("Ability:", res["abilities"][0]["ability"]["name"])
+            id = res["id"]
+            poke_name = res["name"].capitalize()
+            height = res["height"] / 10
+            weight = res["weight"] / 10
 
-            st.write("Base Stats:")
+            types = []
+            for t in res["types"]:
+                types.append(t["type"]["name"].capitalize())
+
+            st.subheader("#" + str(id) + " " + poke_name)
+            st.write("Type: " + " / ".join(types))
+            st.write("Height:", height, "m")
+            st.write("Weight:", weight, "kg")
+
+            st.subheader("Base stats")
+            hp = 0
+            attack = 0
+            defense = 0
+            special_attack = 0
+            special_defense = 0
+            speed = 0
+
             for stat in res["stats"]:
-                st.write(stat["stat"]["name"], ":", stat["base_stat"])
+                stat_name = stat["stat"]["name"]
+                if stat_name == "hp":
+                    hp = stat["base_stat"]
+                if stat_name == "attack":
+                    attack = stat["base_stat"]
+                if stat_name == "defense":
+                    defense = stat["base_stat"]
+                if stat_name == "special-attack":
+                    special_attack = stat["base_stat"]
+                if stat_name == "special-defense":
+                    special_defense = stat["base_stat"]
+                if stat_name == "speed":
+                    speed = stat["base_stat"]
+
+            col1, col2, col3, col4, col5, col6 = st.columns(6)
+            col1.metric("Hp", hp)
+            col2.metric("Attack", attack)
+            col3.metric("Defense", defense)
+            col4.metric("Special Attack", special_attack)
+            col5.metric("Special Defense", special_defense)
+            col6.metric("Speed", speed)
     except requests.HTTPError:
             st.error("Pokémon not found!")
